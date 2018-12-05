f7369547ad7f2984cae73f9e94af62263f4bdb06
Allan Sales <allan.sales.89@gmail.com>
2018-12-05 13:54:55 -0300

    regressao logistica inerente ao modelo

--- logistic_regression_run.py
+++ logistic_regression_run.py
@@ -3,12 +3,8 @@ from joblib import load
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
 
-#import sys
-#data_path = sys.argv[1]
-#model_path = sys.argv[2]
-#output_path = sys.argv[2]
-
 from argparse import ArgumentParser
+import math
 
 parser = ArgumentParser()
 parser.add_argument("-i", "--input", dest="input",
@@ -17,25 +13,39 @@ parser.add_argument("-i", "--input", dest="input",
 parser.add_argument("-o", "--output", dest="output",
                     help="Output file path", metavar="OUTPUT")
 
-parser.add_argument("-m", "--model", dest="model",
-                    help="Model file path", metavar="MODEL")
-
 args = parser.parse_args()
 
 # Data load
 new_data = pd.read_csv(args.input)
 
+column = ['sbp','tobacco','ldl','adiposity','famhist','type','obesity','alcohol','age']
+new_data.columns=column
+
 # Data preprocessing
 encoder = LabelEncoder()
-new_data['famhist']=encoder.fit_transform(new_data['famhist'])
+new_data['famhist'] = encoder.fit_transform(new_data['famhist'])
 scale = MinMaxScaler(feature_range =(0,100))
 new_data['sbp'] = scale.fit_transform(new_data['sbp'].values.reshape(-1,1))
 
 # Load model
-clf = load(args.model) 
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
 
-# Predict heart disease
-pred = clf.predict(new_data)
+## Logit coefficients
+logit_coef = [ 0.00828798,  0.10091803,  0.16580194,  0.04199072, -0.8685583, 0.01429065, -0.1419972 , -0.00107974,  0.02339634]
+logit_intercept = -0.78985061
+
+prob = new_data.multiply(logit_coef, axis=1).sum(axis=1) + logit_intercept
+prob = prob.apply(sigmoid)
+
+#return class of instance
+def define_class(x):
+    if(x < 0.5):
+        return 0
+    return 1
+
+# predict heart disease
+pred = prob.apply(define_class)
 
 # Add prediction to raw data
 new_data = new_data.assign(chd=pred)
